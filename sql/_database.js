4f9f0288f9925c2abcefc4fda1dd500842e841fe
Amandalfs <caduc57@gmail.com>
2023-03-24 16:12:29 -0300

    fix
    Arrumado o error de host name dado por conta do espaco na configuracao do banco de dados usando variaveis de ambiente

--- sql/_database.js
+++ sql/_database.js
@@ -1,13 +1,12 @@
 const pg = require('pg');
 require('dotenv/config');
-
 const client = new pg.Client ({
-    host: process.env.PQ_NAMEHOST,
-    user: process.env.PQ_DATABASE,
-    database: process.env.PQ_HOST,
-    password: process.env.PQ_PASSWORD,
-    port: 5432,
-    ssl: true
+    host:process.env.PQ_HOST,
+    user:process.env.PQ_NAMEHOST,
+    database:process.env.PQ_DATABASE,
+    password:process.env.PQ_PASSWORD,
+    port:5432,
+    ssl:true
 })
 
 module.exports = client;
