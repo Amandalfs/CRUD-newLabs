f1773f09c8ac2d82978217eaaaa77955dd3759c6
Amandalfs <caduc57@gmail.com>
2023-03-27 12:46:11 -0300

    fix
    agora a tabela esta sendo fechada

--- sql/_insertTable.js
+++ sql/_insertTable.js
@@ -3,8 +3,8 @@ const db = require('./_database');
 async function insertTable(){
     await db.connect();
     await db.query(`INSERT INTO dadosnewLabs VALUES('PC Gamer Tier S', 3078.78, false, 4, 'I7 GTX 1650 16gb SSD 250Gb fonte 750W', 5);`)
-    await console.log('Dados inseridos');
     await db.end();
+    await console.log('Dados inseridos');
 
 }
 
--- sql/_select.js
+++ sql/_select.js
@@ -3,8 +3,8 @@ const db = require('./_database');
 async function selectTables(){
     await db.connect();
     const dados = await db.query('SELECT * FROM "public"."dadosnewlabs"');
-    return dados.rows
     await db.end();
+    return dados.rows
 }
 
 module.exports = selectTables;
